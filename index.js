bee9423b39d24dc17bea4cebf9738f64d1001d3e
James Njenga <knjenga94@gmail.com>
2022-08-25 21:49:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+   let text=document.querySelector("#text");
+
+  // console.log(text);
+
+   text.textContent="This is really cool!";
+
+});
